f105ea512ca0f6445285beb81c1242b8f31de89a
Тамир Болданов <rpp-2011@yandex.ru>
2020-10-15 20:13:45 +0300

    my first commit

--- homework.py
+++ homework.py
@@ -1,9 +1,68 @@
 import datetime as dt
-
+date_format = "%d.%m.%Y"
 
 class Calculator:
-    pass
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
 
+    def add_record(self, record):
+        self.records.append(record)
+       
+    def get_today_stats(self):
+        
+        today = dt.datetime.now().date()
+        return sum(
+            record.amount
+            for record in self.records
+            if record.date == today
+        )
+    def get_week_stats(self):
+        
+        today_date = dt.datetime.now().date()
+        seven_days = dt.timedelta(days=7)
+        week_ago = today_date - seven_days
+        return sum(
+            record.amount
+            for record in self.records
+            if week_ago <= record.date <= today_date)
 
 class Record:
-    pass
+    def __init__(self, amount, comment, date=None):
+        self.amount = amount
+        self.comment = comment
+        if date is None:
+            self.date=dt.datetime.now().date()
+        else:
+            self.date = dt.datetime.strptime(date, date_format).date()
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        today_stock_calories = self.limit - self.get_today_stats()
+        if today_stock_calories > 0:
+             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {today_stock_calories} кКал'
+        else:
+                return 'Хватит есть!'
+class CashCalculator(Calculator):
+    USD_RATE = 77.50
+    EURO_RATE = 91.00
+    def get_today_cash_remained(self, currency):
+        currencies = {
+            'rub': (1 , 'руб'),
+            'usd': (self.USD_RATE, 'USD'),
+            'eur': (self.EURO_RATE, 'Euro')
+        }
+        if currency not in currencies.keys():
+            return f'Валюта не поддерживается'
+        balance = self.limit - self.get_today_stats()
+        if balance == 0:
+            return f'Денег нет, держись'
+        balance = round(balance / currencies[currency][0], 2)
+        if balance < 0:
+            balance = abs(balance)
+            return (f'Денег нет, держись: твой долг - '
+                    f'{balance} {currencies[currency][1]}')
+        return (f'На сегодня осталось {balance} '
+                f'{currencies[currency][1]}')
+ 
+if __name__ == "__main__":
